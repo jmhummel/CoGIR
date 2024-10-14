04cff4bf096a29701aab8884bc6d5bbd2c683c2f
Jeremy Hummel <jmhummel@gmail.com>
2024-10-14 16:31:54 -0400

    Init training code

---
+++ train.py
@@ -0,0 +1,51 @@
+import argparse
+from pathlib import Path
+
+import torch
+from torch.utils.data import DataLoader
+import wandb
+
+from models.unet import UNet
+from utils.dataset import ImageDataset
+
+
+def train(image_dir: str):
+    wandb.init(project="CoGIR")
+
+    dataset = ImageDataset(location=Path(image_dir), size=(512, 512))
+    dataloader = DataLoader(dataset, batch_size=2, shuffle=True)
+
+    model = UNet(in_channels=3, out_channels=3)
+    model = model.cuda()
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+    criterion = torch.nn.MSELoss()
+
+    model.train()
+    for epoch in range(10):
+        for i, (inputs, targets) in enumerate(dataloader):
+            inputs = inputs.cuda()
+            targets = targets.cuda()
+
+            outputs = model(inputs)
+
+            loss = criterion(outputs, targets)
+
+            optimizer.zero_grad()
+
+            loss.backward()
+            optimizer.step()
+
+            print(f"Epoch [{epoch+1}/{10}], Loss: {loss.item():.4f}")
+
+            if i % 10 == 0:
+                wandb.log({"loss": loss.item()})
+                input_output_target = torch.cat([inputs, outputs, targets], dim=3)
+                wandb.log({"input_output_target": [wandb.Image(x) for x in input_output_target]})
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("image_dir", type=str)
+    image_dir = parser.parse_args().image_dir
+    train(image_dir)
+
