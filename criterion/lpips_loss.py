37b4f9a5196cf8beaed52ec99358dd710e5c55ff
Jeremy Hummel <jmhummel@gmail.com>
2024-11-22 15:38:48 -0500

    make sure LPIPS params are frozen

--- criterion/lpips_loss.py
+++ criterion/lpips_loss.py
@@ -8,11 +8,12 @@ class LPIPSLoss(nn.Module):
     def __init__(self, use_l1: bool = False):
         super().__init__()
         self.lpips = LPIPS(net='vgg')
+        for p in self.lpips.parameters():
+            p.requires_grad = False
         self.use_l1 = use_l1
 
     def forward(self, x, y):
-        with torch.no_grad():
-            loss = self.lpips(x, y, normalize=True).mean()
-            if self.use_l1:
-                loss += F.l1_loss(x, y)
-            return loss
+        loss = self.lpips(x, y, normalize=True).mean()
+        if self.use_l1:
+            loss += F.l1_loss(x, y)
+        return loss
